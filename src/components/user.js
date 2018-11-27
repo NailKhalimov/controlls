a55c184043147e95b933ac940d523763c3545c5c
Nail` Khalimov <nail980@mail.ru>
2018-11-27 13:08:56 +0300

    Added state controlling

--- src/components/user.js
+++ src/components/user.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
 
+import FormFields from '../widgets/Forms/FormField';
+
 class User extends Component {
 
     state = {
-        
+        formData: {
+            name: {
+                element: 'input',
+                value: '',
+                label: true,
+                labelText: 'Name',
+                config: {
+                    name: 'name_input',
+                    type: 'text',
+                    placeholder: 'Enter your name'
+                }
+            },
+            lastname: {
+                element: 'input',
+                value: '',
+                label: true,
+                labelText: 'LastName',
+                config: {
+                    name: 'lastName_input',
+                    type: 'text',
+                    placeholder: 'Enter your LastName'
+                }
+            }   
+        }
+    }
+
+    updateForm = (newState) => {
+        this.setState({
+            formData: newState
+        })
+    }
+
+    submitForm = (event) => {
+        event.preventDefault();
+
+        let dataToSubmit = {};
+
+        for (let key in this.state.formData) {
+            dataToSubmit[key] = this.state.formData[key].value;
+        }
+        console.log(dataToSubmit)
     }
 
     render(){
         return(
-            <div>
-                User
+            <div className='container'>
+                <form onSubmit={this.submitForm}>
+                    <FormFields
+                        formData={this.state.formData}
+                        change={(newState) => {this.updateForm(newState)}}
+                    />
+                    <button type='submit'>Submit</button>
+                </form>
             </div>
         )
     }
